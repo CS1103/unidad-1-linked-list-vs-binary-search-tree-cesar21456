ead096b1b3ccd0bddbf2bd590bf811304cc3bcf8
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-04-15 00:29:55 -0500

    Update Location.cpp

--- Location.cpp
+++ Location.cpp
@@ -4,4 +4,40 @@
 
 #include "Location.h"
 
-// Defina los metodos que crea necesario
+int Location::get_id(){
+    return position_id;
+}
+string Location::get_state_code(){
+    return state_code;
+}
+string Location::get_county(){
+    return county;
+}
+double Location::get_latitude(){
+    return latitude;
+}
+double Location::get_longitude(){
+    return longitude;
+}
+string Location::get_line(){
+    return line;
+
+}
+string Location::get_construction(){
+    return construction;
+}
+string Location::get_all(){
+    string a;
+    a+=position_id;
+    a+=",";
+    a+=state_code;
+    a+=",";
+    a+=to_string(latitude);
+    a+=",";
+    a+=to_string(longitude);
+    a+=",";
+    a+=line;
+    a+=",";
+    a+=construction;
+    return a;
+}
