6ab33a370a52a6b67420530db8f6794ffb8599b7
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-04-15 00:31:14 -0500

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -5,30 +5,37 @@
 #ifndef LINKEDLISTVSBINARYTREE_BINARYTREE_H
 #define LINKEDLISTVSBINARYTREE_BINARYTREE_H
 #include "Location.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
 
 namespace UTEC {
 
-    struct Node {
+    struct Node2 {
         Location data;
-        Node* right;
-        Node* left;
+        Node2* right;
+        Node2* left;
+        Node2(Location data1):data(data1),right(nullptr),left(nullptr){};
     };
 
     class BinarySearchTree {
-        Node* root;
+        Node2* root;
     public:
         BinarySearchTree();
         ~BinarySearchTree();
         void insert(Location data);
         void clear();
         bool is_empty();
-        Node* get_root();
-        Node* search(std::string position_id);
-        void print();
+        Node2* get_root();
+        Node2* search(int position_id);
+        void print(Node2* b);
+        void deletear_nodos(Node2* a);
     };
 
     void load_locations(BinarySearchTree* linked_list, std::string file_name);
-    void print_node(Node* node);
+    void print_node(Node2* node);
 }
 
 #endif //LINKEDLISTVSBINARYTREE_BINARYTREE_H
